c85286e72883ca176ddaf69a8e7ff5c2de872a81
Yuma Arakawa <yuarkw@gmail.com>
2014-11-12 08:04:35 +0900

    複数のコマンドライン引数に対応

--- honyaku.sh
+++ honyaku.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -s "http://eow.alc.co.jp/search?q=$1" | sed -n '/"resultsList"/,/"navigation"/p' | head -n -1 | w3m -T text/html
+curl -s "http://eow.alc.co.jp/search?q=$(echo $@ | tr ' ' '+')" | sed -n '/"resultsList"/,/"navigation"/p' | head -n -1 | w3m -T text/html
