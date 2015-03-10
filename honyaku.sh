409eda9285506aa9357044f13f5caab0666b3fa3
Yuma Arakawa <yuarkw@gmail.com>
2015-03-10 09:48:54 +0900

    honyaku: ユーザーエージェント設定を追加

--- honyaku.sh
+++ honyaku.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -s "http://eow.alc.co.jp/search?q=$(echo $@ | tr ' ' '+')" | sed -n '/"resultsList"/,/"navigation"/p' | head -n -1 | w3m -T text/html
+curl -A 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:35.0) Gecko/20100101 Firefox/35.0' -s "http://eow.alc.co.jp/search?q=$(echo $@ | tr ' ' '+')" | sed -n '/"resultsList"/,/"navigation"/p' | head -n -1 | w3m -T text/html
